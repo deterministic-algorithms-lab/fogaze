c4adc6141b823cdfd347c766152621b532ed0301
divya-prakash-1 <medivyaprakash@gmail.com>
2020-05-05 10:13:46 +0530

    print in console firebase update

---
+++ init-firebase.js
@@ -0,0 +1,38 @@
+var firebaseConfig = {
+apiKey: "AIzaSyAo6HbDZI4eq1z6OEQzR0OYStcgImDuL9A",
+    authDomain: "fogaze-rx.firebaseapp.com",
+    databaseURL: "https://fogaze-rx.firebaseio.com",
+    projectId: "fogaze-rx",
+    storageBucket: "fogaze-rx.appspot.com",
+    messagingSenderId: "761259327124",
+    appId: "1:761259327124:web:acab8aa284aba9e0e03e81",
+    measurementId: "G-B26C3C66G0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+var database = firebase.database();
+
+var start=document.querySelector('#start');
+
+start.onclick = function start() {
+        firebase.database().ref('/').update({start:"Y"}, function (error) {
+                if (error) {
+                        console.log("The write failed...");
+                } else {
+                        console.log("VIdeo Playing:True")
+                }
+        });
+}
+
+var stop=document.querySelector('#stop');
+stop.onclick = function stop() {
+        firebase.database().ref('/').update({start:"N"}, function (error) {
+                if (error) {
+                        console.log("The write failed...");
+                } else {
+                        console.log("Video playing:False")
+                }
+        });
+}
